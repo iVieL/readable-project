1043fd0912798312407ace2fc3469cae2bf2397d
vlutin <lowrider>
2017-08-31 17:21:01 -0600

    starting with react-redux

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux'
+import {
+  GET_CATEGORIES
+} from '../actions'
+
+function categories(state = {}, action) {
+  switch (action.type) {
+    case GET_CATEGORIES:
+      const { categories } = action
+      return {
+        ...state,
+        categories: categories
+      }
+    default:
+      return state
+  }
+}
+
+export default combineReducers( {
+  categories
+} )
