0cfaba984598a789109f80643d7d3b30859789b2
vlutin <lowrider>
2017-10-02 17:14:01 -0600

    Modal for comments

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react'
-import { Row, Col, Panel, Label } from 'react-bootstrap'
+import { Row, Col, Panel, Label, Button, Modal } from 'react-bootstrap'
 import { formatDate } from '../utils/helpers'
 import { fetchComments } from '../actions'
 import { connect } from 'react-redux'
 import Score from './Score'
 
 class CommentsList extends Component {
+  state = {
+    deleteModalOpen: false,
+    editModalOpen: false
+  }
+
+  openDeleteModal = () => {
+    this.setState(() => ({
+      deleteModalOpen: true,
+      editModalOpen: false
+    }))
+  }
+
+  closeDeleteModal = () => {
+    this.setState(() => ({
+      deleteModalOpen: false
+    }))
+  }
+
+  openEditModal = () => {
+    this.setState(() => ({
+      deleteModalOpen: false,
+      editModalOpen: true
+    }))
+  }
+
+  closeEditModal = () => {
+    this.setState(() => ({
+      editModalOpen: false
+    }))
+  }
+
   componentDidMount() {
     this.props.fetchComments(this.props.postId)
   }
 
   render() {
     const { comments, postId } = this.props
+    const { editModalOpen, deleteModalOpen} = this.state
+
     return (
       <div>
+        <Row>
+          <Col xs={12} sm={12} md={12} lg={12}>
+            <Button
+              className="btn btn-success"
+              onClick={() => this.openEditModal()}
+              >New Comment</Button>
+          </Col>
+        </Row>
         { comments && comments.map( (comment) => (
           <Row key={comment.id}>
             <Col xs={12} sm={12} md={12} lg={12}>
@@ -37,6 +78,25 @@ class CommentsList extends Component {
             </Col>
           </Row>
         ))}
+        { (!comments || comments.length === 0) && (
+          <p>There are no comments! add some!</p>
+        )}
+
+      {/*  New/Edit comment */}
+      <Modal
+        show={editModalOpen}
+        onHide={() => this.closeEditModal()}
+      >
+        <Modal.Header closeButton ><Label className="label label-info">Comment</Label></Modal.Header>
+        <Modal.Body>
+          <span>Tetst edit/new</span></Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.closeEditModal}>Close</Button>
+          {/* <Button bsStyle="primary" onClick={this.deletePost}>Delete!</Button> */}
+        </Modal.Footer>
+      </Modal>
+
+
       </div>
     )
   }
